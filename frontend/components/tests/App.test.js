12673b3a10d846ef645f945c46022f49f34a1ba4
Christopher Ralph <chris.734c@gmail.com>
2024-07-14 15:57:25 -0400

    Completed writing all tests for the various tasks within this project.

--- frontend/components/tests/App.test.js
+++ frontend/components/tests/App.test.js
@@ -1,25 +1,33 @@
 // 👇 YOUR WORK STARTS ON LINE 19
-import React from 'react'
-import { render, waitFor, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import '@testing-library/jest-dom'
-import server from '../../../backend/mock-server'
-import App from '../App'
+import React from 'react';
+import { render, waitFor, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import server from '../../../backend/mock-server';
+import App from '../App';
 
 describe('Stranger Things App', () => {
-  let user
-  afterEach(() => { server.resetHandlers() })
-  beforeAll(() => { server.listen() })
-  afterAll(() => { server.close() })
+  let user;
+  afterEach(() => {
+    server.resetHandlers();
+  });
+  beforeAll(() => {
+    server.listen();
+  });
+  afterAll(() => {
+    server.close();
+  });
   beforeEach(() => {
-    render(<App />)
-    user = userEvent.setup()
-  })
+    render(<App />);
+    user = userEvent.setup();
+  });
   test('App mounts without crashing', () => {
     // 👉 TASK: print the simulated DOM using screen.debug
-  })
+    screen.debug();
+  });
   test('App renders the correct texts', async () => {
     // 👉 TASK: click on the button that displays "Press to Get Show Data"
+    await user.click(screen.getByText('Press to Get Show Data'));
 
     // 👉 TASK: create a waitFor and await for the following to be true:
     //    - The text "Press to Get Show Data" is no longer in the DOM
@@ -27,17 +35,36 @@ describe('Stranger Things App', () => {
     //    - The text "A love letter to the '80s classics that captivated a generation" exists in the DOM
     //    - The text "Select A Season" exists in the DOM
     // ❗ You will need { exact: false } to select the longer text
+    await waitFor(() => {
+      expect(
+        screen.queryByText('Press to Get Show Data')
+      ).not.toBeInTheDocument();
+      expect(screen.getByText('Stranger Things')).toBeInTheDocument();
+      expect(
+        screen.getByText(
+          "A love letter to the '80s classics that captivated a generation",
+          { exact: false }
+        )
+      ).toBeInTheDocument();
+      expect(screen.getByText('Select A Season')).toBeInTheDocument();
+    });
 
     // 👉 TASK: select Season 2 from the dropdown
     // ❗ Don't forget user actions need the await keyword
     // ❗ Use the selectOptions user action
     // ❗ Grab the select element using querySelector
+    await user.selectOptions(document.querySelector('select'), '1');
 
     // 👉 TASK: create the following assertions:
     //    - The text "Season 2, Episode 1" exists in the DOM
     //    - The text "Chapter One: MADMAX" exists in the DOM
     //    - The text "One year after the events with the Upside Down and the Demogorgon" exists in the DOM
     // ❗ You will need { exact: false } to select the longer text
-
-  })
-})
+    screen.getByText('Season 2, Episode 1');
+    screen.getByText('Chapter One: MADMAX');
+    screen.getByText(
+      'One year after the events with the Upside Down and the Demogorgon',
+      { exact: false }
+    );
+  });
+});
